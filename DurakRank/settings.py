a53f0fb420f30f044fcdc6eb688e7f704ee472c7
shayperp <shay.perp@gmail.com>
2022-06-11 14:47:13 +0300

    new branch DEV debug true

--- DurakRank/settings.py
+++ DurakRank/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0b^03i*y^q1$sh_0i-y&6jt^98h7+d)n(7&7hz47_0w9qq--*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'durakrank.herokuapp.com']
 
