3425d36bd24450ed09c714a4d0d6b4e3bfe9167c
shayperp <shay.perp@gmail.com>
2022-07-13 19:36:09 +0300

    deployment

--- DurakRank/settings.py
+++ DurakRank/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0b^03i*y^q1$sh_0i-y&6jt^98h7+d)n(7&7hz47_0w9qq--*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- records/views.py
+++ records/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 # Create your views here.
+from newgame.models import TourN
+from newgame.utility import score_field
 
 
 def index(request):
+    name = None
+    tour = TourN.objects.last()
+    last_game = tour.games_list[-1][score_field]
+    for names in last_game:
+        if names['score'] == 5:
+            name = names['user_name']
+    print(name)
+    champ = {'champion': name}
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', context=champ)
 
 
 def records_page(request):
